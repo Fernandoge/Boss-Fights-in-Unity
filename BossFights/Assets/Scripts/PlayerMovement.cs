8c32f5232bcba638e6b4e05b387916b445976936
Fernandoge <fernando.polgati.g@gmail.com>
2023-07-05 20:55:21 -0400

    Added ninja character with basic animations

--- BossFights/Assets/Scripts/PlayerMovement.cs
+++ BossFights/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -5,24 +6,69 @@ public class PlayerMovement : MonoBehaviour
 {
     private NavMeshAgent navMeshAgent;
     private Camera mainCamera;
+    private Animator anim;
+    private Animation ninjaThrow;
+    private float shootDelay;
 
     private void Awake()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
         mainCamera = Camera.main;
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        if (!Input.GetMouseButtonDown(0)) 
-            return;
+        shootDelay -= Time.deltaTime;  
         
-        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit))
+        if (!navMeshAgent.pathPending)
         {
-            navMeshAgent.SetDestination(hit.point);
+            if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance + 0.25f)
+            {
+                anim.SetBool("Running", false);
+            }
         }
+
+        if (Input.GetMouseButton(1) || Input.GetMouseButton(0)) 
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (Input.GetMouseButton(1))
+                {
+                    anim.SetBool("Running", true);
+                    anim.SetBool("Shooting", false);
+                    navMeshAgent.SetDestination(hit.point);
+                    navMeshAgent.isStopped = false;
+                }
+                if (Input.GetMouseButton(0))
+                {
+                    navMeshAgent.isStopped = true;
+                    if (shootDelay <= 0)
+                    {
+                        Quaternion targetRotation = Quaternion.LookRotation(hit.point - transform.position);
+                        transform.rotation = targetRotation;
+                        anim.SetBool("Running", false);
+                        if (!anim.GetCurrentAnimatorStateInfo(0).IsName("Shoot"))
+                            anim.SetBool("Shooting", true);
+                        else
+                            anim.SetBool("Shooting", false);
+                    }
+                }
+            }
+        }
+    }
+    
+    // Used in Shoot animation
+    public void ShootingEnded()                    
+    {                                              
+        anim.SetBool("Shooting", false);           
+    }
+
+    // Used in Shoot animation
+    public void BasicAttack()
+    {
+        
     }
 }
