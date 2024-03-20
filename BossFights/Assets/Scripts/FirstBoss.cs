21e7b72532287d636c0f3de2515f14db640b47a2
Fernandoge <fernando.polgati.g@gmail.com>
2024-03-20 01:40:52 -0300

    First boss triple smash

--- BossFights/Assets/Scripts/FirstBoss.cs
+++ BossFights/Assets/Scripts/FirstBoss.cs
@@ -3,11 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Serialization;
 
 public class FirstBoss : MonoBehaviour
 {
     public Transform player;
-    public GameObject jumpingAttackParticlePrefab;
+    public GameObject jumpingAttackParticlesPrefab;
+    public GameObject[] tripleSmashParticlesPrefab;
     public float timeBetweenAttacks;
 
     private float auxTimeBetweenAttacks;
@@ -16,6 +18,8 @@ public class FirstBoss : MonoBehaviour
     private Animator _anim;
     private List<IEnumerator> _attacks;
     private bool _jumpingAttack;
+    private bool _tripleSmashAttack;
+    private int _tripleSmashCount;
 
     private Vector3 Direction;
 
@@ -30,7 +34,6 @@ public class FirstBoss : MonoBehaviour
     private void Update()
     {
         timeBetweenAttacks -= Time.deltaTime;
-
         if (timeBetweenAttacks <= 0)
         {
             PerformAttack();
@@ -45,25 +48,82 @@ public class FirstBoss : MonoBehaviour
     private void PerformAttack()
     {
         transform.LookAt(player);
-        StartCoroutine(nameof(JumpAttack));
+        StartJumpAttack();
         timeBetweenAttacks = auxTimeBetweenAttacks;
+    }
+    
+    // --- Jump Attack ---
+
+    private void StartJumpAttack()
+    {
+        jumpingAttackParticlesPrefab.transform.parent = transform;
+        jumpingAttackParticlesPrefab.transform.localPosition = new Vector3();
+        jumpingAttackParticlesPrefab.transform.localRotation = Quaternion.identity;
+        StartCoroutine(JumpAttack());
     }
 
     private IEnumerator JumpAttack()
     {
-        jumpingAttackParticlePrefab.SetActive(false);
         _anim.SetTrigger("JumpAttack");
+        jumpingAttackParticlesPrefab.SetActive(false);
         yield return new WaitUntil(() => _jumpingAttack);
         _playerNavMeshAgent.obstacleAvoidanceType = ObstacleAvoidanceType.NoObstacleAvoidance;
         while (_jumpingAttack)
         {
-            transform.position += transform.forward * Time.deltaTime * 10;
+            transform.position += transform.forward * (Time.deltaTime * 10);
             yield return null; 
         }
         _playerNavMeshAgent.obstacleAvoidanceType = ObstacleAvoidanceType.HighQualityObstacleAvoidance;
     }
 
-    public void JumpAttackStarts() => _jumpingAttack = true;
-    public void JumpAttackEnded() => _jumpingAttack = false;
-    public void ActivateJumpingAttackParticles() => jumpingAttackParticlePrefab.SetActive(true);
+    public void JumpAttackStart() => _jumpingAttack = true;
+    
+    public void JumpAttackEnd() => _jumpingAttack = false;
+
+    public void ActivateJumpingAttackParticles()
+    {
+        jumpingAttackParticlesPrefab.SetActive(true);
+        jumpingAttackParticlesPrefab.transform.parent = transform.parent;
+    }
+
+    public void TriggerTripleSmash() => StartTripleSmash();
+
+    // --- Phase 2: Triple Smash ---
+
+    private void StartTripleSmash()
+    {
+        // if phase 2
+        _tripleSmashCount = 0;
+        foreach (GameObject particlePrefab in tripleSmashParticlesPrefab)
+        {
+            particlePrefab.transform.parent = transform;
+            particlePrefab.transform.localPosition = new Vector3();
+            particlePrefab.transform.localRotation = Quaternion.identity;
+        }
+        StartCoroutine(TripleSmash()); 
+    }
+    
+    private IEnumerator TripleSmash()
+    {
+        if (_tripleSmashCount == 3) 
+            yield break;
+        
+        _tripleSmashAttack = true;
+        tripleSmashParticlesPrefab[_tripleSmashCount].SetActive(false);
+        transform.Rotate(0f, 90f, 0f);
+        _anim.SetTrigger("TripleSmash");
+        yield return new WaitUntil(() => _tripleSmashAttack == false);
+        timeBetweenAttacks = auxTimeBetweenAttacks;
+        _tripleSmashCount++;    
+        
+        StartCoroutine(TripleSmash());
+    }
+    
+    public void TripleSmashEnd() => _tripleSmashAttack = false;
+    
+    public void ActivateTripleSmashParticles()
+    {
+        tripleSmashParticlesPrefab[_tripleSmashCount].SetActive(true);
+        tripleSmashParticlesPrefab[_tripleSmashCount].transform.parent = transform.parent;
+    }
 }
